9e8e1a270bbef37c3ddee11e16d99404fe3cd063
Andrew Edera <iamandrewedera@gmail.com>
2016-06-10 16:14:39 -0700

    Organized for better workflow

--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,16 @@
-<header class="nav-header">
-  <div>
-    <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-       <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="Logo" />
-    </a>
+<header class="header">
+  <div class="nav-header">
+    <div>
+      <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+         <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="Logo" />
+      </a>
+    </div>
+    <nav class="nav-primary">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+      endif;
+      ?>
+    </nav>
   </div>
-  <nav class="nav-primary">
-    <?php
-    if (has_nav_menu('primary_navigation')) :
-      wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-    endif;
-    ?>
-  </nav>
 </header>
