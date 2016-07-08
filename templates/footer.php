008484f2222e6687a5761e87ee9053007dc928b2
Andrew Edera <iamandrewedera@gmail.com>
2016-07-08 08:37:04 -0700

    Dynamic copyright year

--- templates/footer.php
+++ templates/footer.php
@@ -2,4 +2,7 @@
   <div class="container footer-wrapper">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+  <div class="copyright">
+  	Copyright &copy; <?php echo date('Y'); ?>
+  </div>
 </footer>
